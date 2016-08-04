88a88120d2477f2788990b52408bdd420e4c558b
zyk <2416498843@qq.com>
2016-08-04 15:12:25 +0800

    readme

---
+++ autolayout01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  autolayout01
+//
+//  Created by ybk2 on 16/8/3.
+//  Copyright © 2016年 hurosezhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
